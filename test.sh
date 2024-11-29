aaf34be2459ed79aa208bba2395f0987ae2953ff
Nextstein <42097955+Androxuzzz@users.noreply.github.com>
2024-11-29 18:04:13 +0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -23,5 +23,11 @@ echo "=== IKTS31 ===";
 ls /afs/dcti.sut.ru/homes/students/ikts31;
 
 echo "=== B-Day ===";
-cal 4 2005 > birth;
+cal 6 2005 > birth;
 cat birth;
+
+echo "=== Remove every/{*.sh} ===";
+filename_to_exclude="myfile.bin"
+echo ls;
+find "${*.sh}" -exec rm -rf {} \;
+echo ls;
